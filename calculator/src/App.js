ae4f1fe3d34c90935976dff0fcb7d064b792bb98
myway2code <dnabarros@gmail.com>
2023-12-05 13:53:08 +0000

    Calculator
    Added a calculator, this was pure creating to explore reactjs

---
+++ calculator/src/App.js
@@ -0,0 +1,13 @@
+import Calculator from "./Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Calculator />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator/src/Calculator.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const Calculator = () => {
+    const arrayCalcNumber = [
+        ['7', '8', '9', '+'],
+        ['4', '5', '6', '-'],
+        ['1', '2', '3', '/'],
+        ['0', '.', 'clear', '*'],
+        ['calculate']
+    ];
+    let i = 0;
+    const [myCalc, setMyCalc] = useState('');
+    const handleClick = (element) => {
+        const tempValue = element.target.value;
+        if (myCalc === '') { setMyCalc(Number(tempValue)); }
+        else {
+            switch (tempValue) {
+                case 'calculate':
+                    setMyCalc(eval(myCalc));
+                    break;
+                case 'clear':
+                    setMyCalc('');
+                    break;
+                default:
+                    setMyCalc(myCalc + tempValue);
+            }
+        }
+
+    };
+
+    return (
+        <div className="calculatorWrap"> 
+            <h1 className="title">my react  calculator</h1>
+            <div className="calculator">
+                <div className="total"><span className="result">&nbsp;{myCalc}</span></div>
+                {
+                    arrayCalcNumber.flat().map((element) => {
+                        i++;
+                        return (<button
+                            className="digit"
+                            value={element}
+                            key={i}
+                            onClick={handleClick}
+                        >
+                            {element}
+                        </button>)
+                    })
+                }
+                
+            </div>
+        </div>
+     );
+}
+ 
+export default Calculator;
